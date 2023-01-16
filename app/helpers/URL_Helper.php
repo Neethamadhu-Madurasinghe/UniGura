d402717c5d19644a81902ec39ae786ee09a4c12b
Neethamadhu-Madurasinghe <87432896+Neethamadhu-Madurasinghe@users.noreply.github.com>
2023-01-16 13:47:27 +0530

    Add function to redirectUsers Based on their user role

--- app/helpers/URL_Helper.php
+++ app/helpers/URL_Helper.php
@@ -8,4 +8,25 @@ function redirect($path): void {
 }
 
 
+function redirectBasedOnUserRole(Request $request): void {
+    if ($request->isAdmin()) {
+        redirect('admin/dashboard');
+    }elseif ($request->isTutor()) {
+        redirect('tutor/dashboard');
+    }elseif ($request->isStudent()) {
+        redirect('example/dashboard');
+    }elseif ($request->isEmailNotValidatedTutor()) {
+        redirect('tutor/validate-email');
+    }elseif ($request->isEmailNotValidatedStudent()) {
+        redirect('student/validate-email');
+    }elseif ($request->isProfileNotCompletedTutor()) {
+        redirect('tutor/complete-profile');
+    }elseif ($request->isProfileNotCompletedStudent()) {
+        redirect('student/complete-profile');
+    }elseif ($request->isQualificationNotCompletedTutor()) {
+        redirect('/tutor/complete-qualifications');
+    }
+}
+
+
 
