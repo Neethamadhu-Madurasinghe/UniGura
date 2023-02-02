f2efa56f82dd7ba8085d850b9668efa34670abe0
Neethamadhu-Madurasinghe <87432896+Neethamadhu-Madurasinghe@users.noreply.github.com>
2023-02-03 00:19:52 +0530

    Fix css

--- app/views/student/inc/components/MainNavbar.php
+++ app/views/student/inc/components/MainNavbar.php
@@ -3,31 +3,33 @@
 class MainNavbar {
     public static function render(Request $request): void {
         echo '
-            <div class="student-nav-bar">
-                <div class="logo-container">
-                    <a href="' . URLROOT . '/student/dashboard' . '">
-                        <img src="' . URLROOT . '/public/img/common/logo.png' . '" alt="" srcset="" class="nav-logo">
-                    </a>
-                </div>
-                
-                <div class="nav-link-container-container">
-                    <div class="nav-bar-link-container">
-                        <a href="#about">
-                            <div class="icon">
-                                <img src="' . URLROOT . '/public/img/student/bell.png' . '" alt=""> <span>17</span>
-                            </div>
+            <div class="nav-bar-container">
+                <div class="student-nav-bar">
+                    <div class="logo-container">
+                        <a href="' . URLROOT . '/student/dashboard' . '">
+                            <img src="' . URLROOT . '/public/img/common/logo.png' . '" alt="" srcset="" class="nav-logo">
                         </a>
-        
-                        <div class="profile-picture">
-                            <img
-                            src="' . URLROOT . MainNavbar::getStudentProfilePicture($request) . '"
-                            alt="" class="profile-picture-img">
-                            <div class="profile-menu profile-menu-hidden">
-                                <a href="#g">Profile</a>
-                                <a href="' . URLROOT . '/logout' . '">Logout</a>
+                    </div>
+                    
+                    <div class="nav-link-container-container">
+                        <div class="nav-bar-link-container">
+                            <a href="#about">
+                                <div class="icon">
+                                    <img src="' . URLROOT . '/public/img/student/bell.png' . '" alt=""> <span>17</span>
+                                </div>
+                            </a>
+            
+                            <div class="profile-picture">
+                                <img
+                                src="' . URLROOT . MainNavbar::getStudentProfilePicture($request) . '"
+                                alt="" class="profile-picture-img">
+                                <div class="profile-menu profile-menu-hidden">
+                                    <a href="#g">Profile</a>
+                                    <a href="' . URLROOT . '/logout' . '">Logout</a>
+                                </div>
                             </div>
+            
                         </div>
-        
                     </div>
                 </div>
             </div>
