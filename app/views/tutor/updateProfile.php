b40c0a1f6b8dc9446018e32b24ac632c7416d06d
Daylen SK <113536918+Sachithra-dev@users.noreply.github.com>
2023-02-24 11:53:43 +0530

    commited changes

--- app/views/tutor/updateProfile.php
+++ app/views/tutor/updateProfile.php
@@ -15,131 +15,13 @@ Header::render(
     'update profile',
     [
         URLROOT . '/public/css/tutor/base.css?v=1.8',
-<<<<<<< Updated upstream
-        URLROOT . '/public/css/tutor/style.css?v=1.8',
-        URLROOT . '/public/css/tutor/updateProfile.css?v=1.4'
-=======
         URLROOT . '/public/css/tutor/style.css?v=1.9',
         URLROOT . '/public/css/tutor/updateProfile.css?v=1.2'
->>>>>>> Stashed changes
     ]
 );
 MainNavbar::render($request);
 ?>
 
-<<<<<<< Updated upstream
-<div class="main-area-container">
-    <div class="parent">
-        <div class="div1">
-            <form action="update-profile" id="complete-profile-form" method="POST" enctype='multipart/form-data'>
-                <div class="upload-picture-container">
-                    <h1 class="main-title">My Profile</h1>
-                    <img src="<?php echo URLROOT; ?>/public/img/tutor/profile.png" alt="" id="profile-picture">
-                    <input type="file" id="actual-btn" name="profile-picture" accept="image/*" hidden />
-                    <label for="actual-btn" id="profile-image-upload-btn">Upload Profile Picture</label>
-                </div>
-                <div class="tutor-profile">
-                    <form action="updatePassword" method="POST">
-                        <div class="form-field">
-                            <label for="first-name">First Name
-                                <!-- <span>Please enter a valid password</span></label><br> -->
-                                <input type="text" name="currentPassword" id="">
-                        </div>
-                        <div class="form-field">
-                            <label for="last-name">Last Name
-                                <!-- <span>Please enter a valid password</span></label><br> -->
-                                <input type="text" name="newPassword" id="">
-                        </div>
-                        <div class="form-field">
-                            <label for="last-name">Subject
-                                <!-- <span>Password is does't match</span></label><br> -->
-                                <input type="text" name="confirmPassword" id="">
-                        </div>
-                        <div class="form-field">
-                            <label for="last-name">Phone Number
-                                <!-- <span>Password is does't match</span></label><br> -->
-                                <input type="text" name="confirmPassword" id="">
-                        </div>
-                        <div class="form-field">
-                            <label for="last-name">District
-                                <!-- <span>Password is does't match</span></label><br> -->
-                                <input type="text" name="confirmPassword" id="">
-                        </div>
-                        <div class="form-field">
-                            <label for="last-name">City
-                                <!-- <span>Password is does't match</span></label><br> -->
-                                <input type="text" name="confirmPassword" id="">
-                        </div>
-
-                        <div class="change-password">
-                            <button type="submit" class="btn btn-change-password">Save Changes</button>
-                        </div>
-                    </form>
-
-                </div>
-
-        </div>
-
-
-        <div class="div2">
-            <h2>Bank Details</h2><br>
-            <div class="bank-container">
-            <div class="form-field">
-                <label for="last-name">Account Name
-                    <!--  <span><?php echo $data['errors']['last_name_error'] ?></span> -->
-                </label>
-                <input type="text" name="last-name" id="">
-            </div>
-            <div class="form-field">
-                <label for="last-name">Account Number
-                    <!--  <span><?php echo $data['errors']['last_name_error'] ?></span> -->
-                </label>
-                <input type="text" name="last-name" id="">
-            </div>
-            <div class="form-field">
-                <label for="last-name">Bank Name
-                    <!--  <span><?php echo $data['errors']['last_name_error'] ?></span> -->
-                </label>
-                <input type="text" name="last-name" id="">
-            </div>
-            <div class="form-field">
-                <label for="last-name">Branch
-                    <!--<span><?php echo $data['errors']['last_name_error'] ?></span> -->
-                </label>
-                <input type="text" name="last-name" id="">
-            </div>
-        </div>
-        </div>
-        </div>
-
-        <!--===================================== Tutor AVAILABLE TIME SLOTS========================= -->
-        <div class="div4">
-            <h2>Update Your Available Time Slots</h2><br>
-            <div class="pop-time-table">
-                <div class="time-table-container">
-                    <table id="time-table">
-                        <tr class="time-table-titles">
-                            <th id="">Time</th>
-                            <th id="">Monday</th>
-                            <th id="">Tuesday</th>
-                            <th id="">Wednesday</th>
-                            <th id="">Thursday</th>
-                            <th id="">Friday</th>
-                            <th id="">Satday</th>
-                            <th id="">Sunday</th>
-                        </tr>
-
-                        <tr>
-                            <th>8.00-10.00</th>
-                            <td class="slot slot-used"></td>
-                            <td class="slot slot-used"></td>
-                            <td class="slot slot-free"></td>
-                            <td class="slot slot-free"></td>
-                            <td class="slot slot-used"></td>
-                            <td class="slot slot-used"></td>
-                            <td class="slot slot-used"></td>
-                        </tr>
-=======
 <section>
     <div class="main-area-container">
 
@@ -223,7 +105,6 @@ MainNavbar::render($request);
                     </div>
                 </div>
             </div>
->>>>>>> Stashed changes
 
 
 
