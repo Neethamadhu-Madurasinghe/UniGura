6559a1b7fde0f8479272db46450bb1117b0b9791
Daylen SK <113536918+Sachithra-dev@users.noreply.github.com>
2023-02-28 10:37:28 +0530

    changed urlroot

--- app/config/config.php
+++ app/config/config.php
@@ -8,5 +8,5 @@ define('DB_NAME', 'unigura');
 
 define('APPROOT', dirname(dirname(__FILE__)));
 define('ROOT', dirname(dirname(dirname(__FILE__))));
-define('URLROOT', 'http://localhost:81/UniGura');
+define('URLROOT', 'http://localhost/UniGura');
 
