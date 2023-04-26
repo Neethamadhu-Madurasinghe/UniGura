97ae7166dd077ee7d9c77447f513cbc97feda401
Neethamadhu-Madurasinghe <87432896+Neethamadhu-Madurasinghe@users.noreply.github.com>
2023-04-26 19:16:27 +0530

    Refactor email verification

---
+++ app/helpers/OTPHelper.php
@@ -0,0 +1,57 @@
+<?php
+
+
+use PHPMailer\PHPMailer\PHPMailer;
+// Function to generate a random code
+function generateCode(): string {
+    $characters = '0123456789abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ';
+    $string = '';
+
+    for ($i = 0; $i < 6; $i++) {
+        $string .= $characters[mt_rand(0, strlen($characters) - 1)];
+    }
+
+    return $string;
+}
+
+function generateCodeAndSend(Request $request, String $code) {
+    $email = $request->getUserEmail();
+
+    $mail = new PHPMailer(true);
+
+    try {
+        //Server settings                   //Enable verbose debug output
+        $mail->isSMTP();                                            //Send using SMTP
+        $mail->Host       = 'smtp.gmail.com';                     //Set the SMTP server to send through
+        $mail->SMTPAuth   = true;                                   //Enable SMTP authentication
+        $mail->Username   = EMAIL;                     //SMTP username
+        $mail->Password   = PASSWORD;                               //SMTP password
+        $mail->SMTPSecure = PHPMailer::ENCRYPTION_SMTPS;            //Enable implicit TLS encryption
+        $mail->Port       = 465;
+        $mail->SMTPOptions = array(
+            'ssl' => array(
+                'verify_peer' => false,
+                'verify_peer_name' => false,
+                'allow_self_signed' => true
+            )
+        );
+
+        //Recipients
+        $mail->setFrom(EMAIL, 'Unigura');
+        $mail->addAddress($email);               //Name is optional
+
+
+        //Content
+        $mail->isHTML(true);                                  //Set email format to HTML
+        $mail->Subject = 'Unigura: Verify your email';
+        $mail->Body    = '<h1>This is your code for Unigura: ' . $code . '</h1><br>This Code will be expired in 1 hour';
+        $mail->AltBody = 'This is the body in plain text for non-HTML mail clients';
+
+        $mail->send();
+        return true;
+    } catch (Exception $e) {
+        die("Message could not be sent. Mailer Error: {$mail->ErrorInfo}");
+
+    }
+}
+?>
