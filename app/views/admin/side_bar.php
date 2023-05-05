80c31a1fba4c332391b872409ab9be62f6027a28
virajSandakelum <2020cs011@stu.ucsc.cmb.ac.lk>
2023-05-05 16:32:22 +0530

    remove toggle image from the js files

--- app/views/admin/side_bar.php
+++ app/views/admin/side_bar.php
@@ -15,47 +15,47 @@
     <div class="navigation">
         <ul>
             <li class="logo">
-                <img src="./logo.png" alt="">
+                <img src="<?php echo URLROOT ?>/public/img/admin/logo.png" alt="">
             </li>
 
             
-            <li>
+            <li class="nav-link active" id="dashboard">
                 <a href="dashboard">
                     <span class="icon"><i class="fa fa-solid fa-house"></i></span>
                     <span class="title">Home</span>
                 </a>
             </li>
-            <li>
+            <li class="nav-link" id="student">
                 <a href="student">
                     <span class="icon"><i class="fa fa-graduation-cap icon"></i></span>
                     <span class="title">Student</span>
                 </a>
             </li>
-            <li>
+            <li class="nav-link" id="tutor">
                 <a href="tutor">
                     <span class="icon"><i class="fa fa-person-chalkboard icon"></i></span>
                     <span class="title">Tutor</span>
                 </a>
             </li>
-            <li>
+            <li class="nav-link" id="class">
                 <a href="class">
                     <span class="icon"><i class="fa fa-school icon"></i></span>
                     <span class="title">Class</span>
                 </a>
             </li>
-            <li>
+            <li class="nav-link subjectLink" id="subject">
                 <a href="subjectModule">
                     <span class="icon"><i class="fa fa-light fa-book icon"></i></span>
                     <span class="title">Subject</span>
                 </a>
             </li>
-            <li>
+            <li class="nav-link subjectLink" id="statistic">
                 <a href="statistics">
                     <span class="icon"><i class="fa fa-solid fa-chart-simple icon "></i></span>
                     <span class="title">Statistic</span>
                 </a>
             </li>
-            <li>
+            <li class="nav-link" id="request-complaint">
                 <a href="tutorRequest">
                     <span class="icon"><i class="fa fa-solid fa-user-plus"></i></span>
                     <span class="title">Request</span>
@@ -67,20 +67,20 @@
                     <span class="title">Complaint</span>
                 </a>
             </li>
-            <li>
+            <li class="nav-link" id="payment">
                 <a href="payment">
                     <!-- <span class="icon"><i class="fa fa-money-bill-wave icon"></i></span> -->
                     <span class="icon"><i class="fa fa-sharp fa-sack-dollar"></i></span>
                     <span class="title">Payment</span>
                 </a>
             </li>
-            <li>
+            <li class="nav-link" id="notification">
                 <a href="notification">
                     <span class="icon"><i class="fa fa-bell icon"></i></span>
                     <span class="title">Notification</span>
                 </a>
             </li>
-            <li>
+            <li class="nav-link" id="profile">
                 <a href="profileView">
                     <span class="icon"><i class="fa fa-user icon"></i></span>
                     <span class="title">Profile</span>
--- public/js/admin/statistic.js
+++ public/js/admin/statistic.js
@@ -9,15 +9,8 @@ const body = document.querySelector('body'),
     modeText = body.querySelector(".mode-text");
 
 
-const image = document.getElementById("image");
 
 
-toggle.addEventListener("click", () => {
-    sidebar.classList.toggle("close");
-
-    image.src = "images/without-logo.png";
-    image.style.width = "50px";
-})
 
 
 
