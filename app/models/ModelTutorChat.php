b142034927247ffc40b7e257e6cf619354223886
MadushariniRanasinghe12 <madushariniranasinghe@gmail.com>
2023-05-08 12:02:03 +0530

    Tutor chat

---
+++ app/models/ModelTutorChat.php
@@ -0,0 +1,43 @@
+<?php 
+
+class ModelTutorChat {
+    private Database $db;
+
+    public function __construct()
+    {
+        $this->db = new Database();
+    }
+    
+
+
+
+
+
+
+
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+?>
