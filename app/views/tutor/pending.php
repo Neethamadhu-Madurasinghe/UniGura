e148ac0e1dcfe7c2de1833b7ce57ce3dab078e48
Neethamadhu-Madurasinghe <87432896+Neethamadhu-Madurasinghe@users.noreply.github.com>
2023-05-11 23:18:32 +0530

    Add lohout button to pending page

--- app/views/tutor/pending.php
+++ app/views/tutor/pending.php
@@ -9,13 +9,18 @@
 require_once APPROOT . '/views/common/inc/Header.php';
 require_once APPROOT . '/views/common/inc/Footer.php';
 require_once APPROOT . '/views/tutor/inc/components/MainNavbar.php';
+require_once APPROOT . '/views/common/inc/components/IntermediateNavBar.php';
 
 Header::render(
     'Tutor Dashboard',
     [
-        URLROOT . '/public/css/tutor/pendingpage.css'
+        URLROOT . '/public/css/tutor/pendingpage.css',
+        URLROOT . '/public/css/components/intermediate-nav-bar.css',
+        URLROOT . '/public/css/tutor/complete-bank.css?v=2.2'
     ]
 );
+
+IntermediateNavBar::render($request);
 
 ?>
 
@@ -24,7 +29,7 @@ Header::render(
             <h1>Welcome , <?php echo $data['user_name']?></h1>
         </div>
         <hr>
-        <img src="<?php echo URLROOT ?>/public/img/tutor/img2.png" alt="user greet">
+        <img id="pending-img" src="<?php echo URLROOT ?>/public/img/tutor/img2.png" alt="user greet">
         <div class="text">
             <p>Your account is pending and submited for approvel.</p>
         </div>
